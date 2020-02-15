48dd3e5f387c0e63cc71c5477287b012ab5ae57d
Yuta <y.sugiyama54@gmail.com>
2020-02-15 16:46:37 +0900

    記事を作成時にカテゴリを選択して保存できるように修正

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Posts\CreatePostsRequest;
 use App\Http\Requests\Posts\UpdatePostRequest;
 use App\Post;
+use App\Category;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
@@ -26,7 +27,7 @@ class PostsController extends Controller
      */
     public function create()
     {
-        return view('posts.create');
+        return view('posts.create')->with('categories', Category::all());
     }
 
     /**
@@ -49,6 +50,7 @@ class PostsController extends Controller
             'content' => $content,
             'image' => $image,
             'published_at' => $request->published_at,
+            'category_id' => $request->category,
         ]);
 
         session()->flash('success', 'Post created successfully');
@@ -75,7 +77,7 @@ class PostsController extends Controller
      */
     public function edit(Post $post)
     {
-        return view('posts.create')->with('post', $post);
+        return view('posts.create')->with('post', $post)->with('categories', Category::all());
     }
 
     /**
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -46,6 +46,23 @@
                 </div>
 
                 <div class="form-group">
+                    <lavel for="category">Category</lavel>
+                    <select name="category" id="category" class="form-control">
+                        @foreach($categories as $category)
+                            <option value="{{ $category->id }}"
+                                @if (isset($post))
+                                    @if ($category->id == $post->category_id)
+                                        selected
+                                    @endif
+                                @endif
+                            >
+                                {{ $category->name }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="form-group">
                     <button type="submit" class="btn btn-success">
                         {{ isset($post) ? 'Update Post' : 'Create Post' }}
                     </button>
